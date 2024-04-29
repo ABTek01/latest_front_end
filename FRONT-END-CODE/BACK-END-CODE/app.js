84f10da15d7eeb567fa393f75e1cea71c2e7ec82
Aaron Bevans <aaronbevans@unixclone.mynetworksettings.com>
2024-04-28 18:08:13 -0700

    latest commit

---
+++ FRONT-END-CODE/BACK-END-CODE/app.js
@@ -0,0 +1,38 @@
+
+class Media{
+  constructor(title){
+    this._title = title
+    this._isCheckedOut = false
+    this._ratings = []
+  }
+
+  //getter methods
+  get title(){
+    return this._title
+  }
+
+  get isCheckedOut(){
+    return this._isCheckedOut
+  }
+
+  get ratings(){
+    return this._ratings
+  }
+
+  set isCheckedOut(status){
+    return this._isCheckedOut
+  }
+
+  //method that negates checkout property.
+  toggleCheckOutStatus(){
+    this._isCheckedOut = !this._isCheckedOut
+  }
+
+  //method that return the average value of the ratings array.
+  getAverageRating(){
+        //utilize reduce method.
+  }
+
+
+
+}
